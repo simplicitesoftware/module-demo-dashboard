8ca0eb2d4cb4e154a7c9b5fb411bde8ffda573da
designer (Designer) <simplicite@simplicite.fr>
2024-12-13 17:40:27 +0100

    ESLint fixes

--- resources/ObjectExternal/DemoDashboard/SCRIPT.js
+++ resources/ObjectExternal/DemoDashboard/SCRIPT.js
@@ -14,68 +14,68 @@ class DemoDashboard {
 						const count = sts.getField('demoStsCount');
 						const quantity = sts.getField('demoStsQuantities');
 						const total = sts.getField('demoStsTotals');
-			
+
 						$('#DemoDashboard-1').append($ui.view.tools.panel({
 							title: sts.metadata.label + ' (' + count.label + ' / ' + quantity.label + ')',
 							content: $('<div/>', { id: 'DemoDashboard-chart1' })
 						}));
-			
+
 						$('#DemoDashboard-2').append($ui.view.tools.panel({
 							title: sts.metadata.label + ' (' + total.label + ')',
 							content: $('<div/>', { id: 'DemoDashboard-chart2' })
 						}));
-			
+
 						const data1 = new google.visualization.DataTable();
 						data1.addColumn('string', product.label);
 						data1.addColumn('number', count.label);
 						data1.addColumn('number', quantity.label);
-			
+
 						const data2 = new google.visualization.DataTable();
 						data2.addColumn('string', product.label);
 						data2.addColumn('number', total.label);
-			
+
 						sts.search(function(rows) {
 							for (const row of rows) {
 								data1.addRow([ row.demoStsRowId__demoPrdName, row.demoStsCount, row.demoStsQuantities ]);
 								data2.addRow([ row.demoStsRowId__demoPrdName, row.demoStsTotals ]);
 							}
-			
+
 							new google.visualization.BarChart(document.getElementById('DemoDashboard-chart1')).draw(data1, {
 								chartArea: { width: '60%' },
 								vAxis: { title: product.label },
 								bars: 'horizontal'
 							});
-							
+
 							new google.visualization.PieChart(document.getElementById('DemoDashboard-chart2')).draw(data2, {
 								pieHole: 0.2
 							});
 						});
 					});
 				};
-				
+
 				// This chart is using pivot table data
 				const chart3 = d => {
 					$ui.getUIObject('DemoOrder', 'dashboard_DemoOrder', function(ord) {
 						const tab = ord.getPivotTable('DemoOrder-TC4');
-			
+
 						$('#DemoDashboard-3').append($ui.view.tools.panel({
 							title: tab.label,
 							content: $('<div/>', { id: 'DemoDashboard-chart3' })
 						}));
-			
+
 						ord.getPivotTableData(function(cubes) {
 							const data3 = new google.visualization.DataTable();
 							data3.addColumn('string', ord.getField('demoOrdCliId__demoCliCountry').label);
 							data3.addColumn('number', ord.getField('demoOrdTotal').label);
 							data3.addColumn('number', '%');
-			
+
 							let sum = 0;
 							for (const cube of cubes)
 								sum += cube.demoOrdTotal;
-			
+
 							for (const cube of cubes)
-								data3.addRow([ cube.demoOrdCliId__demoCliCountry, cube.demoOrdTotal, cube.demoOrdTotal /  sum * 100 ]);
-							
+								data3.addRow([ cube.demoOrdCliId__demoCliCountry, cube.demoOrdTotal, cube.demoOrdTotal / sum * 100 ]);
+
 							new google.visualization.GeoChart(document.getElementById('DemoDashboard-chart3')).draw(data3, {
 								sizeAxis: { minValue: 0, maxValue: 100 },
 								region: '150', // Europe
@@ -84,7 +84,7 @@ class DemoDashboard {
 						}, tab.name);
 					});
 				};
-			
+
 				// This chart is using a "select" objet
 				const chart4 = d => {
 					$ui.getUIObject('DemoStats1', 'dashboard_DemoStats1', function(sts) {
@@ -92,14 +92,14 @@ class DemoDashboard {
 							title: sts.metadata.label,
 							content: $('<div/>', { id: 'DemoDashboard-chart4' })
 						}));
-			
+
 						const data4 = new google.visualization.DataTable();
 						const status = sts.getField('demoOrdStatus');
 						data4.addColumn('string', status.label);
 						data4.addColumn('number', sts.getField('demoStsCount').label);
 						data4.addColumn('number', sts.getField('demoStsQuantities').label);
 						data4.addColumn('number', sts.getField('demoStsTotals').label);
-			
+
 						sts.search(function(rows) {
 							for (const row of rows) {
 								const rs = Object.values(row); // Transform to an array
@@ -107,7 +107,7 @@ class DemoDashboard {
 								rs[0] = status.getDisplayValue(rs[0]); // Display status value instead of code
 								data4.addRow(rs);
 							}
-			
+
 							new google.visualization.Table(document.getElementById('DemoDashboard-chart4')).draw(data4, {
 								showRowNumber: true,
 								width: '100%', height: '100%'
@@ -115,14 +115,14 @@ class DemoDashboard {
 						});
 					});
 				};
-				
+
 				// This chart is using a custom data provided by the server-side code
 				const chart5 = d => {
 					$('#DemoDashboard-5').append($ui.view.tools.panel({
 						title: d.title,
 						content: $('<div/>', { id: 'DemoDashboard-chart5' })
 					}));
-			
+
 					new google.visualization.Gauge(document.getElementById('DemoDashboard-chart5')).draw(google.visualization.arrayToDataTable([
 						[ 'Label', 'Value' ],
 						[ d.data[0].label, d.data[0].value ],
@@ -134,7 +134,7 @@ class DemoDashboard {
 						minorTicks: 5
 					});
 				};
-	
+
 				google.charts.load('current', { 'packages': ['corechart', 'bar', 'geochart', 'table', 'gauge'], mapsApiKey: Simplicite.GOOGLE_API_KEY });
 				google.charts.setOnLoadCallback(function() {
 					setTimeout(function() { charts1and2(data.chart1, data.chart2); }, 0);
