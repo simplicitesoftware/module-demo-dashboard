f9291ba92f44722cf8764beaa76b0e9f5a34a67e
designer (Designer) <>
2026-07-01 17:01:59 +0200

    Identation again

--- src/com/simplicite/objects/DemoDashboard/DemoStats1.java
+++ src/com/simplicite/objects/DemoDashboard/DemoStats1.java
@@ -6,10 +6,10 @@ import com.simplicite.util.ObjectDB;
  * Statistics per statuses
  */
 public class DemoStats1 extends ObjectDB {
-	private static final long serialVersionUID = 1L;
-	
-	@Override
-	public void postLoad() {
-		setMenuStates(false); // Not configurable
-	}
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void postLoad() {
+        setMenuStates(false); // Not configurable
+    }
 }
